3b02180e83ebcfe28bdcf30b2e775a543520366e
chrisfernandes102 <christopherjoseph.fernandes@calbaptist.edu>
2017-12-02 18:10:56 -0800

    First Push of Project

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -13,28 +13,33 @@ public class HelloController {
     @Autowired
     private VehicleDao vehicleDao;
 
+    @Timed
     @RequestMapping(value = "/addVehicle",method = RequestMethod.POST)
     public Vehicle addVehicle(@RequestBody Vehicle newVehicle) throws IOException{
         vehicleDao.create(newVehicle);
         return newVehicle;
     }
 
+    @Timed
     @RequestMapping(value = "/getVehicle/{id}",method = RequestMethod.GET)
     public Vehicle getVehicle(@PathVariable("id") int id) throws IOException{
         return vehicleDao.getById(id);
     }
 
+    @Timed
     @RequestMapping(value="/updateVehicle", method = RequestMethod.PUT)
     public Vehicle updateVehicle(@RequestBody Vehicle newVehicle) throws IOException {
         return vehicleDao.update(newVehicle);
     }
 
+    @Timed
     @RequestMapping(value="/deleteVehicle/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<String> deleteVehicle(@PathVariable("id") int id) throws IOException {
         vehicleDao.delete(getVehicle(id));
         return new ResponseEntity<String>("Deleted", HttpStatus.OK);
     }
 
+    @Timed
     @RequestMapping(value="/getLatestVehicle", method = RequestMethod.GET)
     public List<Vehicle> getLatestVehicles() throws IOException {
         return vehicleDao.getLatestList("SELECT * FROM vehicles ORDER BY id DESC LIMIT 10");
