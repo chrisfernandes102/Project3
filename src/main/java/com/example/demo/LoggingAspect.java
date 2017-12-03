f8b7eab6cb842cdd9421b744dac725d63abdbcfc
chrisfernandes102 <christopherjoseph.fernandes@calbaptist.edu>
2017-12-02 20:46:53 -0800

    Added FaithfulAspect

--- src/main/java/com/example/demo/LoggingAspect.java
+++ src/main/java/com/example/demo/LoggingAspect.java
@@ -11,15 +11,19 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class LoggingAspect {
-    //For each method in Controller
+    /**
+     * For each method in Controller
+     */
     @Pointcut("execution(public * com.example.demo.HelloController.*(..))")
     public void publicMethod(){}
 
-    //Print which method is being executed
+    /**
+     * Print which method is being executed
+     */
     @Before("publicMethod()")
-    public void addLog(final JoinPoint jP){
-        System.out.println("\n* * * Executing " + jP.getSignature());
-        java.lang.Object[] arguments = jP.getArgs();
+    public void addLog(final JoinPoint joinPoint){
+        System.out.println("\n* * * Executing " + joinPoint.getSignature());
+        java.lang.Object[] arguments = joinPoint.getArgs();
         for(java.lang.Object arg: arguments){
             System.out.println(arg.getClass().getSimpleName() + " = " + arg + "");
         }
